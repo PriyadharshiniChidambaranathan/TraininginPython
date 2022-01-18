75676edc34b363a831e87446db16556d23790db4
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-01-18 18:43:25 +0530

    Update ex-9.py

--- Assessment1/ex-9.py
+++ Assessment1/ex-9.py
@@ -1,5 +1,9 @@
 
-d={'A':1,'B':2,'C':3}
+k=input("Please input a key: ") 
+v=input("Plesse input a value: ") 
+d={k:v} 
+
+#d={'A':1,'B':2,'C':3}
 key=input("key = ")
 if key in d.keys():
       print("Present,value =",end=" ")
