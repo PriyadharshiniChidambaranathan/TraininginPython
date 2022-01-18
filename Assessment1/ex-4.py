35c843f62a61ed9a285cc5143fe9f34dacd49d88
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-01-18 17:17:50 +0530

    Update ex-4.py

--- Assessment1/ex-4.py
+++ Assessment1/ex-4.py
@@ -1,8 +1,8 @@
 import math
 
-numbers = input("Enter D")
+numbers = input("Enter the value for D:")
 numbers = numbers.split(',')
-
+#print(numbers)
 result_list = []
 for D in numbers:
     Q = round(math.sqrt(2 * 50 * int(D) / 30))
