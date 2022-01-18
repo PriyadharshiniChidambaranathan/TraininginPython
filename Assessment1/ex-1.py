a69fa249a45e2eea9151e9d33659914748963139
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-01-18 16:57:15 +0530

    Update ex-1.py

--- Assessment1/ex-1.py
+++ Assessment1/ex-1.py
@@ -1,6 +1,8 @@
-data = ['it', 'finance', 'HR', 'wellness']
-w =input("enter the string:")
-if w in data:
-  print('Found',w)
+XYZ_data = ['Arun', 'Bala', 'Hema', 'Uma','Dev']
+XYZ_data.sort()
+
+data =input("enter the string:")
+if data in XYZ_data:
+  print(data,'Found')
 else:
   print('Not found')
