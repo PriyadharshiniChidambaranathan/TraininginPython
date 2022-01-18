30a8653fa76fb08e73e58a1fbc0fa2550ed9f79d
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-01-18 16:25:33 +0530

    Update ex-3.py

--- Assessment1/ex-3.py
+++ Assessment1/ex-3.py
@@ -1,15 +1,15 @@
 # Factorial of a number using recursion
 
-def recur_factorial(n):
-   if n == 1:
-       return n
+def factorial(num):
+   if (num == 1) or (num==0):
+       return num
    else:
-       return n*recur_factorial(n-1)
+       return num*factorial(num-1)
 
-num = int(input("Enter a number: "))
+number = int(input("Enter a number: "))
 
 # check if the number is negative
-if num < 0:
+if number < 0:
    print("factorial does not exist for negative numbers")
 else:
-   print(recur_factorial(num))
+   print(factorial(number))
