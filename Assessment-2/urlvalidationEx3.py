0f87a62d373ace6d8f3cdfa7f6f3a7637b13cff6
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-02-01 12:44:55 +0530

    Update urlvalidationEx3.py

--- Assessment-2/urlvalidationEx3.py
+++ Assessment-2/urlvalidationEx3.py
@@ -1,1 +1,23 @@
+import re
+
+def ValidateURL(str):
+
+	# Regex to check valid URL
+	regex = ("((http|https)://)(www.)" +"[a-zA-Z0-9@:%._\\+~#?&//=]" +"{2,256}\\.[a-z]" +"{2,6}\\b([-a-zA-Z0-9@:%" +"._\\+~#?&//=]*)")
+	
+	# Compile the ReGex
+	URL = re.compile(regex)
+
+	if (str == None):
+		return False
+	if(re.search(URL, str)):
+		return True
+	else:
+		return False
+
+url =input(" ")
+if(ValidateURL(url) == True):
+	print("Yes")
+else:
+	print("No")
 
