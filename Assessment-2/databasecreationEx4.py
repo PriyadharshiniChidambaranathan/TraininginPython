e8d5848b628486777c6fe7974bc2dcf69874796a
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-02-01 18:08:26 +0530

    Update databasecreationEx4.py

--- Assessment-2/databasecreationEx4.py
+++ Assessment-2/databasecreationEx4.py
@@ -1,1 +1,28 @@
 
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="1234",
+ database="FILMS"
+)
+
+mycursor = mydb.cursor()
+
+#TO CREATE DB
+#mycursor.execute("CREATE DATABASE FILMS")
+
+#TO CREATE TABLE
+mycursor.execute("CREATE TABLE movies (Movie_name VARCHAR(255), Year_of_release VARCHAR(255), Genre VARCHAR(255), Awards_Won VARCHAR(255), Lead_Actor VARCHAR(255), Lead_Actress VARCHAR(255))")
+#mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+
+#to show table created
+#mycursor.execute("SHOW TABLE")
+#for x in mycursor:
+# print(x)
+
+
+
+
+
