4f5dfc830d999990cfd81cf4329e1282d14ad9c7
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-02-01 12:21:19 +0530

    Update urlparsingEx2.py

--- Assessment-2/urlparsingEx2.py
+++ Assessment-2/urlparsingEx2.py
@@ -1,1 +1,17 @@
 
+import re  
+
+
+url=input(" ")
+  
+# finding the protocol 
+protocol = re.findall('(\w+)://',url)
+print("Protocol:",protocol)
+  
+# finding the hostname which may
+hostname = re.findall('://www.([\w\-\.]+)', url)
+print("Hostname:",hostname)
+
+#finding portnum
+portnum = re.findall('(:(\d+))?',url)
+print("Port Number:",portnum)
