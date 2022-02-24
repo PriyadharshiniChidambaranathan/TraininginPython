afb0bd51229a22b733dc077d5da184ac3338951d
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-02-24 22:43:21 +0530

    Update problem 7.py

--- Assessment 4/problem 7.py
+++ Assessment 4/problem 7.py
@@ -1,1 +1,10 @@
 
+import requests 
+
+
+api_url = "https://jsonplaceholder.typicode.com/todos"
+todo = {'userId':'10','title':'macaroni', 'completed':'false'}
+response=requests.post(api_url, json=todo)
+response.json()
+print(response.text)
+response.status_code
