350ce6a97b556c525d94855b3c61810d9d39dee2
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-02-24 23:16:24 +0530

    Update problem 8.py

--- Assessment 4/problem 8.py
+++ Assessment 4/problem 8.py
@@ -1,1 +1,13 @@
 
+import requests 
+import json
+
+
+api_url = "https://jsonplaceholder.typicode.com/todos/10"
+
+data = {'userId':1,'id':10,'Address and pincode':'try_01', 'completed':'true'}
+response=requests.put(api_url,json=data)
+
+
+print(response.text)
+response.status_code
