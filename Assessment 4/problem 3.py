e19529455f68bce2b848f85f185f1399eb2087b7
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-02-24 14:27:08 +0530

    Update problem 3.py

--- Assessment 4/problem 3.py
+++ Assessment 4/problem 3.py
@@ -1,15 +1,32 @@
+
 import pandas as pd
+
 #read the csv file
 df=pd.read_csv(r'/content/drive/MyDrive/nba.csv')
 
 #display 1st 5 rows
 res1=df.head(n=5)
-print (res1)
+print ("\nTop 5 rows: \n",res1)
 
 
 #dimension of desired data
 df_shape = df.shape
-print (df_shape)
+print ("\nDimension:",df_shape)
 
 #type of column datatype
-print(df.dtypes)
+typ=df.dtypes
+print("\nColumn Datatypes:\n",typ)
+
+#display statistics of data
+res_statis=df.describe()
+print("\nStatistics :\n",res_statis)
+
+
+#display median on dataframe
+reult_median=df.median()
+print("\n\nMedian on dataframe:  \n",suii)
+
+#median on age column
+mde_res=df['Age'].median()
+print("\nMedian of Age :", mde_res)
+
