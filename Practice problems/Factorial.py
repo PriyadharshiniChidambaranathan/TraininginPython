29170c6b1376981d249474ee590c6fc2d2994a90
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-03-19 10:27:33 +0530

    Update Factorial.py

--- Practice problems/Factorial.py
+++ Practice problems/Factorial.py
@@ -14,10 +14,18 @@ num=int(input("value:"))
 ans=factorial(num)
 print("Factorial of {} is {}".format(num,ans))
 
-
+-------------------------------------------------------------------------------------------------------------------------------------------------
 #recursion
 def factorial(num):
   if num==0 or num==1:
     return num
  else:
-  return 
+  return num*factorial(num-1)
+
+number = int(input("Enter a number: "))
+
+# check if the number is negative
+if number < 0:
+   print("factorial does not exist for negative numbers")
+else:
+   print(factorial(number))
