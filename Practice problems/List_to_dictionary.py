5720cd7a68ceec39185d24eb7615914995a35b17
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-03-20 20:45:16 +0530

    Create List_to_dictionary.py

---
+++ Practice problems/List_to_dictionary.py
@@ -0,0 +1,11 @@
+def litodict():
+  li=["Sheela","Reena"]
+  id=[289,876]
+  result=dict(zip(li,id))
+  print(result)
+  
+litodict()
+
+----------------------------------
+#       output:
+  #       {'Sheela': 289, 'Reena': 876}
