113dbf9f563589703b13af085df73a6ca300dbcc
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-03-20 12:22:29 +0530

    Create Armstrong.py

---
+++ Practice problems/Armstrong.py
@@ -0,0 +1,18 @@
+val=int(input("Enter val:" ))
+num=val
+res=0
+while(num>0):
+  rem=num%10
+  res=res+(rem**3)
+  num=num//10
+print(res)
+if(res==val):
+  print("{} is an armstrong number".format(val))
+  
+  
+  
+  ------------------------------------------------
+  #output:
+  #     Enter val:153
+  #     153
+  #     153 is an armstrong number
