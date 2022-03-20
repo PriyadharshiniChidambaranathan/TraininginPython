152a5d6ccbe916e598050f8c810b878e12859d33
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-03-20 20:23:09 +0530

    Create CommonLetterBetweenTwoString.py

---
+++ Practice problems/CommonLetterBetweenTwoString.py
@@ -0,0 +1,12 @@
+name1=input("enter name1")
+name2=input("enter name")
+Name1=set(name1)
+Name2=set(name2)
+lst=Name1 & Name2
+print(lst)
+
+-------------
+#output:
+#     enter name1 : Reene
+#     enter name :naina
+#     {'n'}
