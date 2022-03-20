436a12c26ae11b1d08e6085b72a0069888c09403
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-03-20 16:06:28 +0530

    Create reverseList.py

---
+++ Practice problems/reverseList.py
@@ -0,0 +1,7 @@
+list=[1,2,'lu',3,4,'pd',5,6,99]
+print("reverse list is: {}".format(list[::-1]))
+
+
+------------------------------------------
+#output:
+#    reverse list is: [99, 6, 5, 'pd', 4, 3, 'lu', 2, 1]
