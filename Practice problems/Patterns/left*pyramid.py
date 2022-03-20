46295e5cd84bdf30dca236e7221cf4201dcb27ae
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-03-20 14:59:22 +0530

    Create left*pyramid.py

---
+++ Practice problems/Patterns/left*pyramid.py
@@ -0,0 +1,15 @@
+num=5
+m=(2*num)-2
+for i in range(0,num):
+  for j in range(0,i+1):
+    print("*",end=" ")
+  print(" ")
+  
+  
+  ---------------------------------
+  #output:
+#       *  
+ #      * *  
+#       * * *  
+ #      * * * *  
+ #      * * * * *  
