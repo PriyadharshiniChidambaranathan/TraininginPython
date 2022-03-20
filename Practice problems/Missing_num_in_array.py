ee5fd2ed88c7cf0735c986a8a69ae2aee6b06d16
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-03-20 22:47:40 +0530

    Create Missing_num_in_array.py

---
+++ Practice problems/Missing_num_in_array.py
@@ -0,0 +1,28 @@
+
+
+def Get_Missing_Summation():
+  arr1=[1,2,4,5,6,7]
+  n=arr1[-1]
+  sum_of_n=(n*(n+1))//2
+  
+  summ=sum(arr1)
+  res=sum_of_n-summ
+  print(res)
+
+  
+  
+  
+  
+  
+Get_Missing_Summation(a)
+
+
+
+
+--------------------------
+#           Output:
+#             3
+
+-------------------------------
+#using Xor method
+
