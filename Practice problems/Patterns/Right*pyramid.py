9a5d9d7f7e421b02ef3dc36b4ac6dc37120f1ca6
PriyadharshiniChidambaranathan <97425888+PriyadharshiniChidambaranathan@users.noreply.github.com>
2022-03-20 15:03:00 +0530

    Create Right*pyramid.py

---
+++ Practice problems/Patterns/Right*pyramid.py
@@ -0,0 +1,20 @@
+num=5
+m=(2*num)-2
+for i in range(0,num):
+  for j in range(0,m):
+    print(end=" ")
+  m-=1
+  for j in range(0,i+1):
+    print("*",end="")
+  print(" ")
+  
+  
+  
+  ---------------------------------------
+  #output:
+  #     * 
+  #    ** 
+  #   *** 
+  #  **** 
+  # ***** 
+  
